24edd9b8f3f3247008344aa51f10ff4711366030
Vincent <magicdroidx@gmail.com>
2026-05-13 14:12:25 +0000

    Wire bpc to griffe and Jinja with PEP 723 inline deps

--- bpc/__main__.py
+++ bpc/__main__.py
@@ -1,11 +1,48 @@
+#!/usr/bin/env -S uv run --script
+#
+# /// script
+# dependencies = [
+#   "click",
+#   "griffe",
+#   "jinja2",
+# ]
+# ///
+
+from pathlib import Path
+
 import click
+import griffe
+from jinja2 import Environment, FileSystemLoader, StrictUndefined
 
 
 @click.command()
 @click.version_option(version="0.1.0", prog_name="bpc")
 @click.option("-v", "--verbose", is_flag=True, help="Enable verbose output.")
-def main(verbose: bool):
-    pass
+@click.option(
+    "--out",
+    "out_dir",
+    required=True,
+    type=click.Path(file_okay=False, path_type=Path),
+    help="Output directory for generated .hpp files.",
+)
+@click.argument(
+    "inputs",
+    nargs=-1,
+    required=True,
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+)
+def main(verbose: bool, out_dir: Path, inputs: tuple[Path, ...]):
+    env = Environment(
+        loader=FileSystemLoader(str(Path(__file__).parent / "templates")),
+        trim_blocks=True,
+        lstrip_blocks=True,
+        keep_trailing_newline=True,
+        undefined=StrictUndefined,
+    )
+    out_dir.mkdir(parents=True, exist_ok=True)
+    for inp in inputs:
+        mod = griffe.load(inp.stem, search_paths=[str(inp.parent)])
+        print(mod.__repr__())
 
 
 if __name__ == "__main__":
