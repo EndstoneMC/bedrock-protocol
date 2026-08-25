0cf48aaf32269e142c5d548c63d8c0fbe21da0fb
Vincent <magicdroidx@gmail.com>
2026-08-25 22:58:23 +0000

    feat(protocol): model DebugInfoPacket
    A debug string against an actor.

--- protocol/debug.py
+++ protocol/debug.py
@@ -1,4 +1,5 @@
 from protocol import packet
+from protocol.actor import ActorUniqueID
 
 package = "bedrock.protocol"
 
@@ -8,3 +9,9 @@ class EditorNetworkPacket:
     route_to_manager: bool
     raw_variant_name: str
     raw_variant_data: str
+
+
+@packet(id=155, since=2168)
+class DebugInfoPacket:
+    actor_id: ActorUniqueID
+    data: str
