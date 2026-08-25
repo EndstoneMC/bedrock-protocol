dc6b131d2bf883045654d31b2989fa5fd3d63f31
Vincent <magicdroidx@gmail.com>
2026-08-25 18:27:27 +0000

    feat(protocol): model LevelEventPacket
    A world event at a point -- particles, sounds, block break effects -- as an
    event id, the position, and one event-specific data word.

--- protocol/level_event.py
+++ protocol/level_event.py
@@ -1,4 +1,5 @@
-from protocol import packet, uint32
+from protocol import packet, uint32, varint32
+from protocol.common import Vec3
 
 package = "bedrock.protocol"
 
@@ -6,3 +7,10 @@ package = "bedrock.protocol"
 @packet(id=130, since=2168)
 class OnScreenTextureAnimationPacket:
     effect_id: uint32
+
+
+@packet(id=25, since=2168)
+class LevelEventPacket:
+    event_id: varint32
+    pos: Vec3
+    data: varint32
