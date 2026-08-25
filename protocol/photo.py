e6eb0e9798861ed07cdf166e729d5ffa0dcce952
Vincent <magicdroidx@gmail.com>
2026-08-25 21:13:08 +0000

    feat(protocol): model PhotoTransferPacket
    A photo being sent to the client: the image bytes, the book and photo names that
    locate it, and the Education Edition ownership fields that ride along.

--- protocol/photo.py
+++ protocol/photo.py
@@ -1,4 +1,6 @@
-from protocol import field, packet, uint64
+from enum import IntEnum
+
+from protocol import field, int64, packet, uint8, uint64
 from protocol.actor import ActorUniqueID
 
 package = "bedrock.protocol"
@@ -9,3 +11,20 @@ class CreatePhotoPacket:
     id: ActorUniqueID = field(type=uint64)
     photo_name: str
     photo_item_name: str
+
+
+class PhotoType(IntEnum, uint8):
+    PORTFOLIO = 0
+    PHOTO_ITEM = 1
+    BOOK = 2
+
+
+@packet(id=99, since=2168)
+class PhotoTransferPacket:
+    photo_name: str
+    photo_data: str
+    book_id: str
+    type: PhotoType
+    source_type: PhotoType
+    owner_id: ActorUniqueID = field(type=int64)
+    new_photo_name: str
