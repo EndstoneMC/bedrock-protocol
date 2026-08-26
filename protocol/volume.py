fef1bcdb71104585484198796770fc3a114c7962
Vincent <magicdroidx@gmail.com>
2026-08-26 00:44:58 +0000

    feat(protocol): model AddVolumeEntityPacket
    A volume entity entering the world, with its component NBT and the bounds it
    occupies in a named dimension.

--- protocol/volume.py
+++ protocol/volume.py
@@ -1,5 +1,7 @@
 from protocol import packet, uvarint32
 from protocol.attributes import DimensionType
+from protocol.common import BlockPos
+from protocol.nbt import CompoundTag
 
 package = "bedrock.protocol"
 
@@ -11,3 +13,15 @@ type EntityNetId = uvarint32
 class RemoveVolumeEntityPacket:
     entity_net_id: EntityNetId
     dimension_type: DimensionType
+
+
+@packet(id=166, since=2168)
+class AddVolumeEntityPacket:
+    entity_net_id: EntityNetId
+    components: CompoundTag
+    json_identifier: str
+    instance_name: str
+    min_bounds: BlockPos
+    max_bounds: BlockPos
+    dimension_type: DimensionType
+    min_engine_version: str
